33a420d2cf9332c4e6b616180b61f95db93586a4
Marco Cabatingan <marco@lanexus.com>
2019-01-28 18:10:57 +0800

    initial commit

--- js/common.js
+++ js/common.js
@@ -59,19 +59,19 @@ $(function() {
     });
 
     //timeline action
-    $('.info_timeline ul li').mouseenter(function(){
-        $(this).addClass('curr');
-        $(this).parent().addClass('focus');
-        var s_id = $(this).attr('id');
-        $('.info_content').addClass('over')
-        $('.'+s_id).addClass('curr');
-    });
-    $('.info_timeline ul li').mouseleave(function(){
-        $(this).removeClass('curr');
-        $(this).parent().removeClass('focus');
-        $('.info_content').removeClass('over')
-        $('.info_content .desc').removeClass('curr');
-    }); 
+    // $('.info_timeline ul li').mouseenter(function(){
+    //     $(this).addClass('curr');
+    //     $(this).parent().addClass('focus');
+    //     var s_id = $(this).attr('id');
+    //     $('.info_content').addClass('over')
+    //     $('.'+s_id).addClass('curr');
+    // });
+    // $('.info_timeline ul li').mouseleave(function(){
+    //     $(this).removeClass('curr');
+    //     $(this).parent().removeClass('focus');
+    //     $('.info_content').removeClass('over')
+    //     $('.info_content .desc').removeClass('curr');
+    // }); 
 
     //banner
     $('.carousel').carousel({
